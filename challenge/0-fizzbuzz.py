0487095bc13869c3c1256f2a8b9c4c632fe8abcd
NaganoMSL228 <9794@holbertonstudents.com>
2026-06-02 11:45:31 +0200

    test for the task 0

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,9 +1,13 @@
-for i in range(1, n + 1):
-    if (i % 3) == 0 and (i % 5) == 0:
-        tmp_result.append("FizzBuzz")
-    elif (i % 3) == 0:
-        tmp_result.append("Fizz")
-    elif (i % 5) == 0:
-        tmp_result.append("Buzz")
-    else:
-        tmp_result.append(str(i))
+#!/usr/bin/python3
+def fizzbuzz(n):
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    return tmp_result
